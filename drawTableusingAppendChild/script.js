21af45310af48000094daa0058d98c8a1f3e0b4b
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-02 12:36:39 +0530

    Add files via upload

---
+++ drawTableusingAppendChild/script.js
@@ -0,0 +1,35 @@
+// let pageHead= document.createElement("h1");
+// let myElement= document.createElement("span");
+// myElement.innerText="Calendar";
+// pageHead.appendChild(myElement);
+// myElement.classList.add("header");
+// document.body.appendChild(pageHead);
+let myElement= document.createElement("div");
+let pageHeader= document.createElement("h1");
+pageHeader.innerHTML="hello World";
+pageHeader.classList.add("header");
+myElement.appendChild(pageHeader);
+document.body.appendChild(myElement);
+let table= document.createElement("table");
+table.setAttribute("border","1");
+let tableHead= document.createElement("thead");
+tableHead.innerText="Marks Calculator";
+table.appendChild(tableHead);
+document.body.appendChild(table);
+myFunction= () => {
+   
+
+let tableRow= document.createElement("tr");
+let tD1=document.createElement("td");
+let tD2=document.createElement("td");
+tD1.innerHTML="Si No.";
+tD2.innerHTML="Name";
+tableRow.appendChild(tD1);
+tableRow.appendChild(tD2);
+table.appendChild(tableRow);
+}
+let button= document.createElement("button");
+button.innerHTML="Add Tables";
+button.setAttribute("id","buttons");
+document.body.appendChild(button);
+document.getElementById("buttons").addEventListener("click", myFunction);
