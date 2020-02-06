4aa45c4f9ed68ae6ae3d08e536503d90178a5288
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-06 12:17:49 +0530

    Update longestWordinAString.js

--- longestWordinAString.js
+++ longestWordinAString.js
@@ -1,11 +1,20 @@
 function longestWord(str){
 let words= str.split(" ");
-let longestWord= "";
- for (let word of words){
- if(word.length>longestWord.length){
- longestWord=word;
+let lWord= [""];
+ let size=0;
+ for (let i=0; i<words.length-1;i++){
+ if(words[i].length>size){
+  size= words[i].length;
+  if(lWord[lWord.length-1].length< words[i].length){
+   lWord=[];
+   lWord.push(words[i]);
+  }
+  else{
+   lWord=[...lWord,words[i]];
  }
  }
- return longestWord;
+ }
+ return [...lWord];;
 }
- console.log("this is the longest word");
+ console.log(longestWord("this is the longest word"));
+console.log(longestWord("I woke up early today"));
